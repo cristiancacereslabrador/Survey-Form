b31b68cba2fba5d699ac65eb746dcfb212f02892
Cristian Vianey Cáceres Labrador <80643642+cristiancacereslabrador@users.noreply.github.com>
2022-07-18 15:54:15 -0500

    Add files via upload

--- send_mail.php
+++ send_mail.php
@@ -3,22 +3,22 @@ if (isset($_POST)) {
     $name = $_POST['name'];
     $email = $_POST['email'];
     $subject = $_POST['subject'];
-    $comment = $_POST['comment'];
+    $comments = $_POST['comment'];
     $domain = $_SERVER['HTTP_HOST'];
     $to = "cristianvianey@gmail.com";
-    $subject = "Contacto desde el formulario del sitiuo $domain: $subject";
+    $subject_mail = "Contacto desde el formulario del sitiuo $domain.";
     $message = "
     <p> Datos enviados desde el formulario del sitio <b>$domain</b></p>
     <ul>
         <li>Nombre: <b>$name</b></li>
         <li>Email: <b>$email</b></li>
         <li>Asunto: $subject</li>
-        <li>Comentarios: $subject</li>
+        <li>Comentarios: $comments</li>
     </ul>
     ";
     $headers = "MIME-Version: 1.0" . "\r\n" . "Content-Type: text/html; charset=utf-8" . "\r\n" . "From: Envío Automático No responder <no-reply@$domain>";
 
-    $send_mail = mail($to, $subject, $message, $headers);
+    $send_mail = mail($to, $subject_mail, $message, $headers);
 
     if($send_mail){
         $res = [
